32b96baf1f6eddd9b47e81720d7840c42c75c7ad
JilMunSu <sh466sh@gmail.com>
2021-03-01 23:45:13 +0900

    add: 투표기능 추가

---
+++ commands/server/suggestions.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: '투표',
+    aliases: [],
+    permissions: [],
+    description: '투표를 하자!',
+    execute(client, message, cmd, args, Discord){
+        const channel = message.guild.channels.cache.find(c => c.name === 'bot');
+        if(!channel) return message.channel.send('투표채널이 존재하지 않습니다.');
+
+        let messageArgs = args.join(' ');
+        const embed = new Discord.MessageEmbed()
+        .setColor('FADF2E')
+        .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+        .setDescription(messageArgs);
+
+        channel.send(embed).then((msg) =>{
+            msg.react('👍');
+            msg.react('👎');
+            message.delete();
+        }).catch((err)=>{
+            throw err;
+        });
+    }
+}
