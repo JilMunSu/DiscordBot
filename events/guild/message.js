7886407cd340538b9d4a1d419d87f00ff148471d
JilMunSu <sh466sh@gmail.com>
2021-03-01 23:43:52 +0900

    feat: permissions 관련 기능 추가

--- events/guild/message.js
+++ events/guild/message.js
@@ -34,7 +34,55 @@ module.exports = async (Discord, client, message) => {
     const cmd = args.shift().toLowerCase();
     const command = client.commands.get(cmd) || client.commands.find(a => a.aliases && a.aliases.includes(cmd));
 
-    // commands 파일에 없는 명령어의 이름일 경우 catch로 안가고 name이 없다고 뜸
+    const validPermissions = [
+        "CREATE_INSTANT_INVITE",
+        "KICK_MEMBERS",
+        "BAN_MEMBERS",
+        "ADMINISTRATOR",
+        "MANAGE_CHANNELS",
+        "MANAGE_GUILD",
+        "ADD_REACTIONS",
+        "VIEW_AUDIT_LOG",
+        "PRIORITY_SPEAKER",
+        "STREAM",
+        "VIEW_CHANNEL",
+        "SEND_MESSAGES",
+        "SEND_TTS_MESSAGES",
+        "MANAGE_MESSAGES",
+        "EMBED_LINKS",
+        "ATTACH_FILES",
+        "READ_MESSAGE_HISTORY",
+        "MENTION_EVERYONE",
+        "USE_EXTERNAL_EMOJIS",
+        "VIEW_GUILD_INSIGHTS",
+        "CONNECT",
+        "SPEAK",
+        "MUTE_MEMBERS",
+        "DEAFEN_MEMBERS",
+        "MOVE_MEMBERS",
+        "USE_VAD",
+        "CHANGE_NICKNAME",
+        "MANAGE_NICKNAMES",
+        "MANAGE_ROLES",
+        "MANAGE_WEBHOOKS",
+        "MANAGE_EMOJIS",
+      ]
+    
+      if(command.permissions.length){
+        let invalidPerms = []
+        for(const perm of command.permissions){
+          if(!validPermissions.includes(perm)){
+            return console.log(`Invalid Permissions ${perm}`);
+          }
+          if(!message.member.hasPermission(perm)){
+            invalidPerms.push(perm);
+          }
+        }
+        if (invalidPerms.length){
+          return message.channel.send(`Missing Permissions: \`${invalidPerms}\``);
+        }
+      }
+      
     if(!cooldowns.has(command.name)){
         cooldowns.set(command.name, new Discord.Collection());
     }
