562c4596859f53109ddd98929f814863b328a4e7
JilMunSu <sh466sh@gmail.com>
2021-03-01 22:24:18 +0900

    feat: 예금 관련 메시지 출력

--- commands/server/balance.js
+++ commands/server/balance.js
@@ -3,6 +3,6 @@ module.exports = {
     permissions: [],
     description: "총액 확인 시스템",
     execute(client, message, cmd, args, Discord, profileData) {
-      message.channel.send(`당신은 ${profileData.coins}원을 가지고있습니다.`); //you banks bal is ${profileData.bank}
+      message.channel.send(`당신은 ${profileData.coins}원을 가지고있습니다. \n당신의 계좌에 ${profileData.bank}원이 있습니다.`);
     },
   };
